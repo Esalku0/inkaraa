c9a96aa7ece0055898c30314a77966bfb067f792
Estanis <estanisaliagak@gmail.com>
2025-03-25 09:06:05 +0100

    Añadiendo rutas de acceso de la aplicación web

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservaCitaComponent } from './reserva-cita/reserva-cita.component';
+import { LoginComponent } from './login/login.component';
+import { VerDisenyosComponent } from './ver-disenyos/ver-disenyos.component';
+import { RegisterComponent } from './register/register.component';
+import { VerArtistasComponent } from './ver-artistas/ver-artistas.component';
+import { PantallaGestionComponentArtista } from './pantalla-gestion-artista/pantalla-gestion.component-artista';
+
+export const routes: Routes = [
+  
+  { path: '', component: HomeComponent },
+  { path: 'reserva', component: ReservaCitaComponent },
+  { path: 'registro', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'verDisenyos', component: VerDisenyosComponent },
+  { path: 'verArtistas', component: VerArtistasComponent },
+  {path: 'gestionArtista', component: PantallaGestionComponentArtista},
+  
+  { path: '**', redirectTo: '' },
+];

238babd701d57f00cd74b8bb9bdef5a58f28326e
Estanis <estanisaliagak@gmail.com>
2025-03-25 09:06:50 +0100

    Añadiendo services de usuarios y artistas

---
+++ src/app/service/artistas.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Artista } from '../POJOs/artistas';
+@Injectable({
+  providedIn: 'root',
+})
+export class ArtistasService {
+  private API = 'http://localhost:3000/artistas';
+  constructor(private httpClient: HttpClient) {}
+
+  getAllArtistas() {
+    console.log(this.httpClient.get(this.API));
+    return this.httpClient.get(this.API);
+  }
+
+  getAllArtistasById(id: number) {
+    console.log('este es el id a buscar ' + id);
+    return this.httpClient.get(this.API + '/' + id);
+  }
+  getArtistasByEstilo(id: number) {}
+
+  
+  addArtista(formData: FormData) {
+    console.log(formData);
+
+    return this.httpClient.post(this.API, formData);
+  }
+  
+
+}

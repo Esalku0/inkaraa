2f738fb2255c53dbd1e9466b6a8a30e69dee880c
Estanis <estanisaliagak@gmail.com>
2025-04-14 12:48:45 +0200

    cambios menores

--- src/app/reserva-cita/reserva-cita.component.ts
+++ src/app/reserva-cita/reserva-cita.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Reserva } from '../POJOs/reservas';
+import { ReservasService } from '../service/reservas.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CarrouselImagenesComponent } from '../carrousel-imagenes/carrousel-imagenes.component';
 
 @Component({
   selector: 'app-reserva-cita',
-  imports: [],
+  imports: [CommonModule,
+    FormsModule,
+    MatIconModule,
+    RouterLink,
+    RouterOutlet,
+    CarrouselImagenesComponent],
   templateUrl: './reserva-cita.component.html',
   styleUrl: './reserva-cita.component.css'
 })
 export class ReservaCitaComponent {
 
+  idUsuario: number = Number(localStorage.getItem("idUsuario"));
+  myDate = new Date();
+
+  newReserva: Reserva = {
+    idReserva: 0,
+    idArtista: 0,
+    idCliente: this.idUsuario,
+    fechaReserva: this.myDate,
+    detalles: '',
+    boceto: '',
+    idEstado: 0
+  }
+
+  reservaService: ReservasService = inject(ReservasService);
+
+  constructor() {
+
+  }
+
+  anyadirReserva() {
+
+  }
+
+  cargarArtistas(){
+    
+  }
 }
